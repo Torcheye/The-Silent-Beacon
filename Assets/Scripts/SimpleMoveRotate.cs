59ba25eccd5bfafbf95c9a69ecc7f6615a5a8f28
King Zhou <zhouyimingking@outlook.com>
2024-10-20 14:26:50 -0500

    volumetric light

---
+++ Assets/Scripts/SimpleMoveRotate.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class SimpleMoveRotate : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float moveMagnitude = 1f;
+    public float rotateSpeed = 100f;
+    public bool moveHorizontally = true;
+    public bool moveVertically = false;
+    public bool enableRotation = false;
+
+    private Vector3 startPosition;
+    private float movementFactor;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (moveHorizontally)
+        {
+            movementFactor = Mathf.Sin(Time.time * moveSpeed) * moveMagnitude;
+            transform.position = startPosition + new Vector3(movementFactor, 0, 0);
+        }
+        
+        if (moveVertically)
+        {
+            movementFactor = Mathf.Sin(Time.time * moveSpeed) * moveMagnitude;
+            transform.position = startPosition + new Vector3(0, movementFactor, 0);
+        }
+
+        if (enableRotation)
+        {
+            transform.Rotate(new Vector3(0, rotateSpeed * Time.deltaTime, 0));
+        }
+    }
+}
